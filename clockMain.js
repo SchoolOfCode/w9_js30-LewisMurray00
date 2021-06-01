c5b439abc334a89278b1435a77ade441c03f0a07
Lewis Murray <lewisdmurray00@gmail.com>
2021-06-01 21:59:17 +0100

    Second and minute hand

---
+++ clockMain.js
@@ -0,0 +1,21 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+
+function setDate(){
+    //console.log('hi');
+    const now = new Date();
+    
+    const seconds = now.getSeconds();
+    const secondDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60)*360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hourDegrees = ((hours / 60)*360) + 90;
+
+}
+
+setInterval(setDate, 1000);
