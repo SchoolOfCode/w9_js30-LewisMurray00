ef6be20a7e716d52e878bdcffb4b3e9368073d95
Lewis Murray <lewisdmurray00@gmail.com>
2021-06-03 20:25:24 +0100

    Added the api into an array called cities

--- .github/Type_Ahead/main.js
+++ .github/Type_Ahead/main.js
@@ -1,8 +1,13 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
+//let cities = [];
+//.then(data => cities = data)
+
 const cities = [];
 
-//ANother way to use the basic fetch request
+//Another way to use the basic fetch request
 fetch(endpoint)
     .then(response => response.json())
-    .then(data => console.log(data));
+    //pushes the data into the cities array, ... needed as its the spread
+    .then(data => cities.push(...data));
+    
