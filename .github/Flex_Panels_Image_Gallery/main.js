f7527394058b70fa80a699d07479535044f7fba1
Lewis Murray <lewisdmurray00@gmail.com>
2021-06-03 19:46:49 +0100

    Added a function that waits for the end of the transistion then checks what has transitioned, if it contains flex then it will change the class to open-active returning the first and final childs

--- .github/Flex_Panels_Image_Gallery/main.js
+++ .github/Flex_Panels_Image_Gallery/main.js
@@ -4,4 +4,12 @@ function toggleOpen(){
     this.classList.toggle('open')
 }
 
+function toggleActive(e){
+    console.log(e.propertyName);
+    if(e.propertyName.includes('flex')){
+        this.classList.toggle('open-active');
+    }  
+}
+
 panel.forEach(panel => panel.addEventListener('click', toggleOpen));
+panel.forEach(panel => panel.addEventListener('transitionend', toggleActive));
