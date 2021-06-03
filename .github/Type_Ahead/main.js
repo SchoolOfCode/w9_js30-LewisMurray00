a4f51df43bfb8cb24a3838f9a6a4074d1757f9c6
Lewis Murray <lewisdmurray00@gmail.com>
2021-06-03 20:35:07 +0100

    Linked the search bar to the dom, created an event listener for change which then runs the display matches function

--- .github/Type_Ahead/main.js
+++ .github/Type_Ahead/main.js
@@ -1,3 +1,6 @@
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
 //let cities = [];
@@ -19,5 +22,13 @@ function findMatches(wordToMatch, cities){
         const regex = new RegExp(wordToMatch, 'gi');
         return place.city.match(regex) || place.state.match(regex);
         
-    })
+    });
 }
+
+function displayMatches(){
+    console.log(this.value);
+
+}
+
+searchInput.addEventListener('change', displayMatches);
+
