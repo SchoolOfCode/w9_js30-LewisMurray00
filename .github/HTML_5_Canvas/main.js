759fb7f17e7d1b637937c87da291c3fce998494c
Lewis Murray <lewisdmurray00@gmail.com>
2021-06-04 17:52:14 +0100

    Context added and added width to the screeen to match the wdith and height of the window

---
+++ .github/HTML_5_Canvas/main.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector('#draw');
+
+//context needed to be drawn on
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
