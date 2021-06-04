63906ddcc57bc9f45d04f0cacef260518828acdf
Lewis Murray <lewisdmurray00@gmail.com>
2021-06-04 17:54:39 +0100

    Added stroke style and line join to the java

--- .github/HTML_5_Canvas/main.js
+++ .github/HTML_5_Canvas/main.js
@@ -4,3 +4,9 @@ const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+//Added for the canvas
+ctx.strokeStyle = '#BADA55'
+//if a line meets another it becomes rounded off
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
